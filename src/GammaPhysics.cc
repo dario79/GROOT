ebbc351fd25293622b85142fbaa1c85176b2fc44
Alessandro Alberto Oliva <alessandro.oliva@lns.infn.it>
2024-06-14 11:18:37 +0200

    fixed GammaPhysics

--- src/GammaPhysics.cc
+++ src/GammaPhysics.cc
@@ -35,6 +35,8 @@
 #include "G4BinaryCascade.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4ProcessManager.hh"
+#include "G4PhotoNuclearCrossSection.hh"
+
 
 // Processes
 
@@ -60,7 +62,7 @@ void GammaPhysics::ConstructProcess()
 {
     G4ProcessManager* pManager = G4Gamma::Gamma()->GetProcessManager();
     G4HadronInelasticProcess* process = new G4HadronInelasticProcess("photonNuclear", G4Gamma::Definition());
-    photonNuclear->AddDataSet( new G4PhotoNuclearCrossSection );
+    process->AddDataSet( new G4PhotoNuclearCrossSection );
 
     G4CascadeInterface* theCascade = new G4CascadeInterface();
     theCascade->usePreCompoundDeexcitation();
