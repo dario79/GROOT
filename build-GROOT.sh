102c683348d1d85bd54488bbadca59b71ba546f2
Alessandro Alberto Oliva <50516076+alexoliva91@users.noreply.github.com>
2024-06-14 07:33:13 +0200

    Update build-GROOT.sh

--- build-GROOT.sh
+++ build-GROOT.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source /opt/geant4/install/bin/geant4.sh
+source /opt/root/install/bin/thisroot.sh
+
 cd /opt/GROOT/build
 cmake -DCMAKE_INSTALL_PREFIX=/opt/GROOT/install -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF ../source
 make -j$(nproc)
